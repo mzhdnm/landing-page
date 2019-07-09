e266c15566037a4ea8665e612b495df0e620e1d1
Artem Suschev <Suschev.Artem@gmail.com>
2019-07-09 17:52:13 +0300

    appied template

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,19 @@
-module.exports = {
-  dir: {
-    input: "source",
-    output: "_rendered",
-  },
-  templateFormats: [
-    "md",
-  ]
+module.exports = function(config) {
+  // Passthrough copy
+  config.addPassthroughCopy('source/assets');
+  config.addPassthroughCopy('source/images');
+  config.addPassthroughCopy('LICENSE.txt');
+  config.addPassthroughCopy('robots.txt');
+
+  return {
+    dir: {
+      input: 'source',
+      output: 'dist'
+    },
+    templateFormats: [
+      'md',
+      'njk',
+    ],
+    passthroughFileCopy: true
+  };
 };
--- source/index.md
+++ source/index.md
@@ -1,1 +1,6 @@
-lolkkk
+---
+layout: index.njk
+title: Между Нами
+subtitle: Лучшие каши на этом полушарии
+message: Кус-кус, киш-миш и ням-ням.  
+---
