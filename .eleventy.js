a91a7db1aca472eb15397da18c448cc8d39610a4
Artem Suschev <Suschev.Artem@gmail.com>
2019-10-24 12:55:57 +0300

    fixed e11y config

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,9 @@ module.exports = function(config) {
   // Passthrough copy
   config.addPassthroughCopy('source/assets');
   config.addPassthroughCopy('source/images');
-  config.addPassthroughCopy('LICENSE.txt');
-  config.addPassthroughCopy('robots.txt');
-  config.addPassthroughCopy('CNAME');
+  config.addPassthroughCopy('source/LICENSE.txt');
+  config.addPassthroughCopy('source/robots.txt');
+  config.addPassthroughCopy('source/CNAME');
 
   return {
     dir: {
